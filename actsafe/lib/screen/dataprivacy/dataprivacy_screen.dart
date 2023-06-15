1b595fbd6374e1f7e24a53c6e6a6603a5fccdfce
dianasupangan <diananicole.supangan@benilde.edu.ph>
2023-06-15 15:57:29 +0800

    data privacy photo

--- actsafe/lib/screen/dataprivacy/dataprivacy_screen.dart
+++ actsafe/lib/screen/dataprivacy/dataprivacy_screen.dart
@@ -13,16 +13,26 @@ class DataPrivacyScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Data Privacy"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: [
-            DataPrivacyNotes(),
-            InformationCollected(),
-            DataRetention(),
-            Agreement(),
-          ],
-        ),
+      body: ListView(
+        children: [
+          Image.asset("assets/images/dataPrivacyPage.png"),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: const DataPrivacyNotes(),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: const InformationCollected(),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: const DataRetention(),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: const Agreement(),
+          ),
+        ],
       ),
     );
   }
